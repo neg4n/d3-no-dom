724aae7a05a1a6411aa6fa17928b55da9528bca9
Igor Klepacki <neg4n@icloud.com>
2024-06-18 01:41:17 +0200

    feat: reduce dependencies amount

--- src/index.ts
+++ src/index.ts
@@ -1,66 +1,67 @@
-import * as d3 from "d3";
 import type { PartialDeep, RequiredDeep } from "type-fest";
-import { JSDOM, type ConstructorOptions as JsDomOptions } from "jsdom";
+import type {  ConstructorOptions as JsDomOptions } from "jsdom";
 import { safeHtml } from "common-tags";
 import { dset as deepSet } from "dset";
 import { isString, mergeDeep } from "remeda";
 
-type PrepareSvgServerSideRenderOptions = {
-  svg?: PartialDeep<{
+
+type PrepareSvgServerSideRendererParams = {
+  jsdomInstance: typeof import("jsdom").JSDOM;
+  d3Instance:  typeof import("d3");
+  options?: {
+    dom?: JsDomOptions;
+  };
+};
+
+type PrepareSvgServerSideRender = ({
+  currentDom,
+  d3Selection,
+  svgNode,
+}: {
+  currentDom: import("jsdom").JSDOM;
+  d3Selection: import("d3").Selection<SVGSVGElement, unknown, null, undefined>;
+  svgNode: SVGSVGElement;
+}) => Promise<void> | Promise<string>;
+
+type PrepareSvgServerSideRenderOptions = PartialDeep<{
+  svg: {
     width: number;
     height: number;
-  }>;
-  safe?: boolean;
-  asBase64?: boolean;
-  dom?: JsDomOptions;
-};
+  };
+  safe: boolean;
+  asBase64: boolean;
+}>;
 
-type PrepareSvgServerSideRenderParams = {
-  options?: PrepareSvgServerSideRenderOptions;
-};
-
-type SvgServerSideRenderParams = {
-  dom: JSDOM;
-  d3: typeof d3;
-  svg: SVGSVGElement;
-};
-
-type PrepareSvgServerSideRendererFn = ({
-  dom,
-  d3,
-  svg,
-}: SvgServerSideRenderParams) => Promise<void> | Promise<string>;
-
-const DEFAULT_OPTIONS: PrepareSvgServerSideRenderOptions = {
-  svg: {
-    width: 100,
-    height: 100,
-  },
-  safe: true,
-  asBase64: false,
-  dom: {
-    pretendToBeVisual: true,
+const DEFAULT_OPTIONS: Pick<PrepareSvgServerSideRendererParams, "options"> = {
+  options: {
+    dom: {
+      pretendToBeVisual: true,
+    },
   },
 };
+
+const DEFAULT_RENDER_OPTIONS: RequiredDeep<PrepareSvgServerSideRenderOptions> =
+  {
+    svg: {
+      width: 100,
+      height: 100,
+    },
+    safe: true,
+    asBase64: false,
+  };
 
 export function prepareSvgServerSideRenderer({
-  options: incomingOptions = {},
-}: PrepareSvgServerSideRenderParams) {
-  const options = mergeDeep(
-    DEFAULT_OPTIONS,
-    incomingOptions,
-  ) as RequiredDeep<PrepareSvgServerSideRenderOptions>;
+  options: incomingRendererOptions = {},
+  d3Instance,
+  jsdomInstance,
+}: PrepareSvgServerSideRendererParams) {
+  const rendererOptions = mergeDeep(DEFAULT_OPTIONS, incomingRendererOptions);
 
-  const dom = new JSDOM("<body></body>", options.dom);
-  const body = d3.select(dom.window.document.body);
+  const dom = new jsdomInstance("<body></body>", rendererOptions.dom);
+  const body = d3Instance.select(dom.window.document.body);
 
   const createPaintingArea = () =>
-    body
-      .append("svg")
-      .attr("id", "painting-area")
-      .attr("width", options.svg.width)
-      .attr("height", options.svg.height)
-      .node()!;
+    body.append("svg").attr("id", "painting-area").node()!;
 
   const svgElement = createPaintingArea();
 
@@ -80,11 +81,31 @@ export function prepareSvgServerSideRenderer({
     },
   });
 
-  const render = async (fn: PrepareSvgServerSideRendererFn) => {
-    const svg = options.safe ? safeSvgElement : svgElement;
-    const result = await fn({ dom, svg, d3 });
+  const render = async (
+    fn: PrepareSvgServerSideRender,
+    options: PrepareSvgServerSideRenderOptions,
+  ) => {
+    const renderOptions = mergeDeep(
+      DEFAULT_RENDER_OPTIONS,
+      options,
+    ) as RequiredDeep<PrepareSvgServerSideRenderOptions>;
 
-    if (options.asBase64) {
+    const svg = renderOptions.safe ? safeSvgElement : svgElement;
+    const d3SelectedSvg = d3Instance.select(svg);
+
+    d3SelectedSvg
+      .attr("width", renderOptions.svg.width)
+      .attr("height", renderOptions.svg.height);
+
+    const result = await fn({
+      currentDom: dom,
+      svgNode: svg,
+      d3Selection: d3SelectedSvg,
+    });
+
+    d3SelectedSvg.html(null).attr("width", null).attr("height", null);
+
+    if (renderOptions.asBase64) {
       if (isString(result)) return toSvgBase64(result);
       return toSvgBase64(svg.outerHTML);
     }
@@ -92,14 +113,8 @@ export function prepareSvgServerSideRenderer({
     return isString(result) ? result : svg.outerHTML;
   };
 
-  const clear = async () => {
-    const svg = options.safe ? safeSvgElement : svgElement;
-    svg.innerHTML = "";
-  };
-
   return {
     render,
-    clear,
   };
 }
 
